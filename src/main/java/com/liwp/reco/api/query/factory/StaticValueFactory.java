0048e60dfe3a50c6d1cd0deb666cf80784aaf2f6
liwp <327647669@qq.com>
2018-07-02 13:38:32 +0800

    clean

--- src/main/java/com/liwp/reco/api/query/factory/StaticValueFactory.java
+++
@@ -1,9 +0,0 @@
-package com.liwp.reco.api.query.factory;
-
-/**
- * Created by liwp on 2017/5/16.
- */
-public class StaticValueFactory {
-    public static String LUCENE_CORE_BASEURL = "http://lucene.apache.org/core/6_4_1/core";
-
-}
